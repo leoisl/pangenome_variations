2c605f9d39917737c0c64138a6d2a3739954c666
leandro <leandro@ebi.ac.uk>
2020-05-06 16:09:51 +0100

    Adding submit_lsf.sh script

---
+++ scripts/submit_lsf.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+MEMORY=8000
+PROFILE="lsf"
+LOG_DIR=logs/
+JOB_NAME="snakemake_master_process."$(date --iso-8601='minutes')
+
+mkdir -p $LOG_DIR
+
+bsub -R "select[mem>$MEMORY] rusage[mem=$MEMORY] span[hosts=1]" \
+     -M "$MEMORY" \
+     -o "$LOG_DIR"/"$JOB_NAME".o \
+     -e "$LOG_DIR"/"$JOB_NAME".e \
+     -J "$JOB_NAME" \
+     snakemake --profile "$PROFILE" --stats "$LOG_DIR"/snakemake_stats "$@"
+
+exit 0
