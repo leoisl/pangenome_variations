24ac18ebc4d55a2abd690750a76ec3e6bb03e088
leandro <leandro@ebi.ac.uk>
2020-08-18 11:54:02 +0100

    Fixing small argument passing bug on run_pipeline_local.sh

--- scripts/run_pipeline_local.sh
+++ scripts/run_pipeline_local.sh
@@ -4,5 +4,5 @@ set -eux
 LOG_DIR=logs/
 mkdir -p "$LOG_DIR"
 
-snakemake --use-singularity >"$LOG_DIR"/pipeline.out 2>"$LOG_DIR"/pipeline.err
+snakemake --use-singularity "$@" >"$LOG_DIR"/pipeline.out 2>"$LOG_DIR"/pipeline.err
 exit 0
