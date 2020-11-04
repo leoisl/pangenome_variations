ac22713d120a1f9074ad0b189f75abe626431c8a
leandro <leandro@ebi.ac.uk>
2020-11-04 19:48:27 +0000

    Cleanup: removing test.py

--- scripts/test.py
+++
@@ -1,3 +0,0 @@
-from src.VarifierDataframe import VarifierDataframe
-
-df = VarifierDataframe.load_pickled("/home/leandro/git/pangenome_variations/analysis_output/snps_dfs/CFT073/CFT073_and_H131800734.snps_df.pickle")
